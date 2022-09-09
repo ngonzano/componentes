8fc3c593a9546e94525feee7aeb2e3964baf57b3
ngonzano <110738507+ngonzano@users.noreply.github.com>
2022-09-09 13:26:58 -0500

    todo bien

--- lib/widgets/boton_animado.dart
+++ lib/widgets/boton_animado.dart
@@ -13,21 +13,19 @@ class _BotonAnimadoState extends State<BotonAnimado> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(),
-      body: Center(
-        child: InkWell(
-          onTap: () {
-            setState(() {
-              enable = !enable;
-            });
-          },
-          child: SizedBox(
-            width: 100,
-            height: 100,
-            child: AnimatedContainer(
-              curve: Curves.decelerate,
-              duration: const Duration(seconds: 1),
-              color: enable ? Colors.green : Colors.black,
-            ),
+      body: InkWell(
+        onTap: () {
+          setState(() {
+            enable = !enable;
+          });
+        },
+        child: SizedBox(
+          width: 100,
+          height: 100,
+          child: AnimatedContainer(
+            curve: Curves.decelerate,
+            duration: const Duration(seconds: 1),
+            color: enable ? Colors.green : Colors.black,
           ),
         ),
       ),
